ac22e057f40bf116c57727c2e19017a0bda31251
duyleomessi <duyleitbka95@gmail.com>
2016-02-20 01:18:19 +0700

    add FavoritesController and favoriteFilter

--- Ionic/conFusion/www/js/controllers.js
+++ Ionic/conFusion/www/js/controllers.js
@@ -11,7 +11,7 @@ angular.module('conFusion.controllers', [])
 
     // Form data for the login modal
     $scope.loginData = {};
-    $scope.reservation = {}; 
+    $scope.reservation = {};
 
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -61,10 +61,10 @@ angular.module('conFusion.controllers', [])
         $timeout(function() {
             $scope.closeReserve();
         }, 1000);
-    };   
+    };
 }])
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
     $scope.baseURL = baseURL;
     $scope.tab = 1;
     $scope.filtText = '';
@@ -103,6 +103,12 @@ angular.module('conFusion.controllers', [])
     $scope.toggleDetails = function() {
         $scope.showDetails = !$scope.showDetails;
     };
+
+    $scope.addFavorite = function(index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
+    }
 }])
 
 .controller('ContactController', ['$scope', function($scope) {
@@ -239,4 +245,48 @@ angular.module('conFusion.controllers', [])
     $scope.leaders = corporateFactory.query();
     console.log($scope.leaders);
 
-}]);
+}])
+
+.controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+
+    $scope.baseURL = baseURL;
+    $scope.shouldShowDelete = false;
+
+    $scope.favorites = favoriteFactory.getFavorites();
+
+    $scope.dishes = menuFactory.getDishes().query(
+        function(response) {
+            $scope.dishes = response;
+        },
+        function(response) {
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+        });
+    console.log($scope.dishes, $scope.favorites);
+
+    $scope.toggleDelete = function() {
+        $scope.shouldShowDelete = !$scope.shouldShowDelete;
+        console.log($scope.shouldShowDelete);
+    }
+
+    $scope.deleteFavorite = function(index) {
+
+        favoriteFactory.deleteFromFavorites(index);
+        $scope.shouldShowDelete = false;
+
+    }
+}])
+
+.filter('favoriteFilter', function() {
+    return function(dishes, favorites) {
+        var out = [];
+        for (var i = 0; i < favorites.length; i++) {
+            for (var j = 0; j < dishes.length; j++) {
+                if (dishes[j].id === favorites[i].id)
+                    out.push(dishes[j]);
+            }
+        }
+        return out;
+
+    }
+});
+
