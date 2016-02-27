94b730196b8646884573ffa461f783501697f750
duyleomessi <duyleitbka95@gmail.com>
2016-02-27 21:50:05 +0700

    add resolve for homepage aboutpage

--- Ionic/conFusion/www/js/app.js
+++ Ionic/conFusion/www/js/app.js
@@ -55,7 +55,20 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
                 views: {
                     'mainContent': {
                         templateUrl: 'templates/home.html',
-                        controller: 'IndexController'
+                        controller: 'IndexController',
+                        resolve: {
+                            dish: ['menuFactory', function(menuFactory) {
+                                return menuFactory.get({id: 0});
+                            }],
+
+                            promotion: ['promotionFactory', function(promotionFactory) {
+                                return promotionFactory.get({id: 0});
+                            }],
+
+                            leader: ['corporateFactory', function(corporateFactory) {
+                                return corporateFactory.get({id: 3});
+                            }]
+                        }
                     }
                 }
             })
@@ -65,7 +78,12 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
                 views: {
                     'mainContent': {
                         templateUrl: 'templates/aboutus.html',
-                        controller: 'AboutController'
+                        controller: 'AboutController',
+                        resolve: {
+                            leaders: ['corporateFactory', function(corporateFactory) {
+                                return corporateFactory.query();
+                            }]
+                        }
                     }
                 }
             })
@@ -85,7 +103,12 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
                 views: {
                     'mainContent': {
                         templateUrl: 'templates/menu.html',
-                        controller: 'MenuController'
+                        controller: 'MenuController',
+                        resolve: {
+                            dishes: ['menuFactory', function(menuFactory) {
+                                return menuFactory.query();
+                            }]
+                        }
                     }
                 }
             })
